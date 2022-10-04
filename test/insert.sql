fafbb9c68770f0828702f3a5bf5dc873e1775afe
Massimo Nocentini <massimo.nocentini@gmail.com>
2022-10-04 15:32:42 +0200

    Add the restore_db command

--- test/insert.sql
+++ test/insert.sql
@@ -1,6 +1,8 @@
 
 -- simple insertions.
 
+--/usr/local/pgsql/bin/pg_restore --host=localhost --port=5437 --username=pdm --dbname=pdmCC --verbose /var/lib/pgadmin/storage/massimo.nocentini_gmail.com/pdmCC20220921.backup
+
 INSERT INTO pdm.replicationlog(
 	cchistoryid, createtime, applytime, applyduration, command)
 	VALUES (1, current_timestamp, NULL, NULL, '{}'::jsonb);
