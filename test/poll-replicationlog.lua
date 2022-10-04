e50faac976423085fada912ab5f09962e160025b
Massimo Nocentini <massimo.nocentini@gmail.com>
2022-10-04 10:37:43 +0200

    Fix the polling of the  table

--- test/poll-replicationlog.lua
+++ test/poll-replicationlog.lua
@@ -1,6 +1,10 @@
 
+local curl = require 'curl'
+
 -- load driver
 local luasql = require "luasql.postgres"
+
+local timestamp_pattern = "(%d+)-(%d+)-(%d+) (%d+):(%d+):(%d+).(%d+)"
 
 -- create environment object
 local env = assert (luasql.postgres())
@@ -13,27 +17,76 @@ local row = cur:fetch ({}, "a")
 local _, max_createtime = next(row)
 cur:close()
 
-if not max_createtime then max_createtime = '0' end
-
+-- adjust the initial createtime in case of an empty `replicationlog` table.
+if not max_createtime then max_createtime = '2020-01-01 00:00:00.000000' end
 print('Initial max CREATETIME: ' .. max_createtime)
 
-while true do
-	os.execute 'sleep 1s'
+local function apptivegrid_upload (entity_json)
+		
+	return function (cu)
+		local returns, getinfos = curl.curl_easy_httpheader_setopt_getinfo {
+			httpheader	= { 
+				['Content-Type'] = 'application/json',
+			},
+			setopt		= {	
+				url = 'https://app.apptivegrid.de/api/users/6315f0a9f5ca3bb794a42cb3/spaces/6315f0b667d3ac2664a44f52/grids/631ee7590cb7e1473fa4c5ee/entities?layout=property',
+				verbose = true,
+				header = false,
+				postfields = entity_json,
+				cainfo = 'curl-ca-bundle.crt',
+				netrc = curl.opt_netrc.CURL_NETRC_OPTIONAL,
+			},
+			getinfo 	= { 
+				response_code = true, 
+			}
+		} (cu)
+		
+		local code, response_code = getinfos.response_code()
+		assert(code == curl.CURLcode.CURLE_OK)
+		assert(response_code == 201)
+	end
+end
 
-	-- retrieve a cursor
-	cur = assert (con:execute ("SELECT * from pdm.replicationlog WHERE createtime > '" ..
-					max_createtime .. "' ORDER BY createtime ASC;"))
-	-- print all rows, the rows will be indexed by field names
+while true do
+	os.execute 'sleep 2s'
+
+	-- get all new rows inserted during the sleep.
+	local query = "SELECT * from pdm.replicationlog WHERE createtime > '"
+			.. max_createtime .. "' ORDER BY createtime ASC;"
+	cur = assert (con:execute (query))
+
 	row = cur:fetch ({}, "a")
 	while row do
-	  --print(string.format("Name: %s, E-mail: %s", row.name, row.email))
 	  print (row.createtime)
 	  if max_createtime <= row.createtime then max_createtime = row.createtime end
 
-	  -- reusing the table of results
-	  row = cur:fetch (row, "a")
+		local matcher = string.gmatch(row.createtime, timestamp_pattern)
+
+		local y, M, d, h, m, s, ss = matcher()
+
+		local entity_json = string.format([[
+
+		  {
+		    "created": "%s-%s-%sT%s:%s:%s+02:00",
+		    "replicationkey": 123,
+		    "locationoutgoing": 456,
+		    "locationincoming":789,
+		    "commandurl": [],
+		    "attachmentcount": 0,
+		    "status": "new",
+		    "errortext": null
+		  }
+
+		]], y, M, d, h, m, s)
+		
+		print('Inserting: \n' .. entity_json .. '\n')
+			  
+	  curl.curl_easy_do (apptivegrid_upload (entity_json))
+
+	  row = cur:fetch (row, "a")	-- go to the next row.
 	end
-	cur:close()
+
+	cur:close()	-- cleanup the cursor for the current query.
 end
 
 -- close everything
